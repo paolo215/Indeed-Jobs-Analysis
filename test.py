59b534628d8c9db4033d1a8a581db97e045acc73
Paolo Villanueva <paolo_villanueva215@yahoo.com>
2017-11-19 12:23:40 -0500

    Updated test for database.py

--- test.py
+++ test.py
@@ -1,24 +1,34 @@
+from database import DB_Manager
 
-class Test():
-    pass
+db = DB_Manager()
 
-class B():
-    def __init__(self, test):
-        self.b = test
+def insert_tests():
+    db.insert_tech("Python1")
+    db.insert_tech("Python2")
+    db.insert_tech("Python3")
+    db.insert_tech("Python4")
+    db.insert_tech("Python5")
+
+    db.insert_job("1", "Tester1", "1", "1", "1", "1")
+    db.insert_job("1", "Tester2", "1", "1", "1", "1")
+    db.insert_job("1", "Tester3", "1", "1", "1", "1")
+    db.insert_job("1", "Tester4", "1", "1", "1", "1")
+
+    db.insert_keyword("Keyword1")
+    db.insert_keyword("Keyword2")
+    db.insert_keyword("Keyword3")
+    db.insert_keyword("Keyword4")
 
 
-class A():
-    def __init__(self):
-        self.a = Test()
-        self.b = B(self.a)
+    db.insert_jobs_tech("1", "1")
+    db.insert_jobs_tech("2", "2")
+    db.insert_jobs_tech("3", "1")
 
-    def test(self):
-        self.a = None
 
-a = A()
-print(a.a)
-print(a.b)
-a.test()
-print(a.a)
-print(a.b)
+def query_test():
+    rows = db.get_all_tech()
+    print(rows)
 
+
+query_test()
+db.close()
